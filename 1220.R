a843fb00b27516cbb9f222063f3067caed762f59
LAPTOP-U59K2599\ASUS <a0987708266@gmail.com>
2023-12-20 19:39:10 +0800

    完整的學習控制區的互動參數

---
+++ 1220.R
@@ -0,0 +1,12 @@
+##               Functions            Inputs
+## 1  checkboxGroupInput()   selected values
+## 2       checkboxInput()           logical
+## 3           dateInput()              date
+## 4      dateRangeInput()           2 dates
+## 5           fileInput()         file name
+## 6        numericInput()           numeric
+## 7        radioButtons()    selected value
+## 8         selectInput() selected value(s)
+## 9         sliderInput()           numeric
+## 10       submitButton()           numeric
+## 11          textInput()         character
